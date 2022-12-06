2c8beca88c015efd13301405d2af6c5a3e3d75d4
闫玉东-Mac <yanyudong0904@163.com>
2022-12-06 18:39:25 +0800

    初始化了解流程

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, MarkdownEditView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -14,20 +14,47 @@ export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
+		console.log("plugin is onload")
 		await this.loadSettings();
 
+		// 左侧的图标按钮
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
+			console.log("this.app", this.app);
+			console.log("this.app.workspace.getLayout", this.app.workspace.getLayout());
+			console.log("this.app.vault.getMarkdownFiles", this.app.vault.getMarkdownFiles());
+			// new Notice(JSON.stringify(this.app));
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
+		// const ribbonIconEl1 = this.addRibbonIcon('dice', 'reload', (evt: MouseEvent) => {
+		// 	// Called when the user clicks the icon.
+		// 	this.load();
+		// });
+
+		// 底部的一行字样
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('Status Bar Text Fuck');
 
+		// 注册事件  命令行触发
+		this.addCommand({
+			id: 'show-editor-object',
+			name: 'show editor object',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				console.log("editor.getDoc", editor.getDoc())
+				console.log("editor.getValue", editor.getValue())
+				// editor.replaceRange("我用什么把你留住", { line: 0, ch: 0 })
+				console.log("view", view)
+				view.setViewData("哈哈哈哈", false)
+				// console.log("view", view.setViewData("哈哈哈哈", false))
+			}
+		});
+
+		// 注册事件  命令行触发, 打开弹框
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
@@ -36,6 +63,8 @@ export default class MyPlugin extends Plugin {
 				new SampleModal(this.app).open();
 			}
 		});
+
+		// 注册事件  命令行触发，将当前选中的文本替换为Sample editor command
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'sample-editor-command',
@@ -45,6 +74,8 @@ export default class MyPlugin extends Plugin {
 				editor.replaceSelection('Sample Editor Command');
 			}
 		});
+
+
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
 			id: 'open-sample-modal-complex',
@@ -65,13 +96,16 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
+		// 配置项的标签添加
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
+		// 注册全局点击事件触发
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
 			console.log('click', evt);
+			// new Notice('This is a notice!');
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
@@ -91,22 +125,24 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
+// 弹框
 class SampleModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
 
 	onOpen() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.setText('Woah!');
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
+// 配置项的样式
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
@@ -116,11 +152,11 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' });
 
 		new Setting(containerEl)
 			.setName('Setting #1')
